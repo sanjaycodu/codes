f493aea1be60e80e60a84c89b2e85ade1bce6067
sanjaysidjay <sanjaydhoni13@gmail.com>
2024-09-22 07:11:15 -0700

    Feat:

--- Linkedlists/Linkedls.java
+++ Linkedlists/Linkedls.java
@@ -4,17 +4,38 @@ public class  Linkedls {
         int data;
         Node next=null;
         Node(int val){data=val; next=null;}
-          //  System.out.println("hai");}
     }Linkedls(){head=null;}
 
     void addListFirstPlace(int val) {
         Node newnode = new Node(val);
+      //  Node temp=head;
         if (head == null) {
             head = newnode;
         } else {
             newnode.next = head;
             head = newnode;
         }
+    }
+    void addSpecificpos(int pos,int val){
+        Node newnode = new Node(val);
+            Node temp=head;
+        for (int i = 1; i < pos; i++) {
+            temp=temp.next;
+            if(temp==null){
+                throw new IllegalArgumentException("Invalid pos: "+pos) ;
+            }
+        }
+        newnode.next=temp.next;
+        temp.next=newnode;
+    }
+    void deleteSpecficpos(int pos){
+       Node temp=head;
+       Node prev=null;
+        for (int i = 1; i <=pos; i++) {
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=temp.next;
     }
       public void display(){
             Node temp=head;
