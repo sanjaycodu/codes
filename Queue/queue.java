7cee9738459cb9905f2f8f68d6ebbf14ac6c4484
sanjaysidjay <sanjaydhoni13@gmail.com>
2024-10-05 09:10:49 -0700

    Queue using shifting arrays

--- Queue/queue.java
+++ Queue/queue.java
@@ -3,34 +3,38 @@ public class queue {
         queueDemo queue=new queueDemo();
         queue.enqeue(5);
         queue.enqeue(6);
-        queue.display();
-        queue.dequeue();
-        queue.display();
+         queue.display();
+         queue.dequeue();
+         queue.display();
     }
     }
      class queueDemo{
         static final int MAX_SIZE=20;
         int[] arr=new int[MAX_SIZE];
-        int fornt=-1;
+        int fornt=0;
         int rear=-1;
         void enqeue(int val){
-            if(fornt==-1)
-                fornt++;
             if(rear==MAX_SIZE-1)
                 throw new IndexOutOfBoundsException("queue is full");
             arr[++rear]=val;
         }
         void dequeue(){
-            if(fornt==-1 || fornt>rear)
+            if(rear==-1)
                 throw new IndexOutOfBoundsException("queue is empty");
-            else{
-            System.out.println(arr[fornt++]);
-
+            else{ 
+            int temp=arr[0];
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+                rear--;
+            }
+            System.out.println(temp+" is dequeued");
             }
         }
         void display() {
+            System.out.println("-----------Qeue has---------- ");
             for (int i = fornt; i <=rear; i++) {
-                System.out.print(arr[i]+" ");
+                System.out.println(arr[i]+" ");
             }
+            System.out.println("");
         }
     }
